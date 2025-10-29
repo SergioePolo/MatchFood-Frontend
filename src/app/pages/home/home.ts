e6520617217f93b53657cebee3d056963e123a73
Sergio Polo <129100808+DeveloperFundonal@users.noreply.github.com>
2025-10-28 23:00:36 -0500

    [create] Creación de las páginas home y perfil de restaurante

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Carousel } from "../../components/carousel/carousel";
-import { Card } from "../../components/card/card"; 
+
 @Component({
   selector: 'app-home',  
-  imports: [Carousel, Card],
+  imports: [Carousel],
   templateUrl: './home.html',
   styleUrls: ['./home.css']  
 })
--- src/app/pages/restaurant-profile/restaurant-profile.ts
+++ src/app/pages/restaurant-profile/restaurant-profile.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ServiceRestaurants } from '../../services/restaurants';
+import { Restaurant } from '../../interfaces/restaurant';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-restaurant-profile',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './restaurant-profile.html',
   styleUrl: './restaurant-profile.css'
 })
-export class RestaurantProfile {
+export class RestaurantProfile implements OnInit{
+  _restaurantService = inject(ServiceRestaurants);
+  restaurantId: string | null = null;
+  restaurant?: Restaurant;
 
+  constructor(private route: ActivatedRoute) {}
+
+  getRestaurantDetails(id: string){
+    this._restaurantService.searchRestaurantById(id).subscribe({
+      next:(res: any)=>{
+        this.restaurant = res.data;
+      },
+      error:(error:any)=>{
+        console.log(error)
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    this.restaurantId = this.route.snapshot.paramMap.get('id'); 
+    if (this.restaurantId) {
+      this.getRestaurantDetails(this.restaurantId);
+    }
+  }
 }
